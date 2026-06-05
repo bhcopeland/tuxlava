411e5e183bc1aba2e27e2381752e6059dc8f7523
Ben Copeland <ben.copeland@linaro.org>
2026-06-05 09:52:12 +0100

    devices: add LAVADevice base for lava job-definition passthrough
    Move the shared passthrough logic, load and validate a LAVA job
    definition and return it unchanged, into a new LAVADevice base in
    devices/lava.py.
    
    No functional changes.
    
    Signed-off-by: Ben Copeland <ben.copeland@linaro.org>

--- tuxlava/devices/fvp.py
+++ tuxlava/devices/fvp.py
@@ -9,8 +9,6 @@
 from typing import Any, Dict, List, Optional
 
 import re
-import urllib
-import yaml
 from tuxlava import templates
 from tuxlava.devices import Device
 from tuxlava.exceptions import InvalidArgument
@@ -333,27 +331,3 @@ class FVPMorelloGrub(MorelloFVPDevice):
     kernel_start_message = "Press enter to boot the selected OS"
     prompts = ["highlighted entry will be executed"]
     support_tests = True
-
-
-class FVPLAVA(FVPDevice):
-    name = "fvp-lava"
-
-    def validate(self, job_definition, **kwargs):
-        if not job_definition:
-            raise InvalidArgument("Missing argument --job-definition")
-        parsed_url = urllib.parse.urlparse(job_definition)
-        job_definition = urllib.parse.unquote(parsed_url.path)
-        with open(job_definition, "r") as job_file:
-            try:
-                # Load yaml and dump data as string to verify that
-                # lava job definition is valid
-                yaml_data = yaml.dump(yaml.safe_load(job_file))
-                self.job_definition = yaml_data
-            except Exception:
-                raise InvalidArgument("Unable to load LAVA job definition")
-        return
-
-    def default(self, options) -> None: ...  # noqa: E704
-
-    def definition(self, **kwargs):
-        return self.job_definition
